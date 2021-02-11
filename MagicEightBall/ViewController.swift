237e64fa4dc769588e0f26127a138031e695e9e1
sicaramacuya <eric.morales-rodriguez@students.makeschool.com>
2021-02-10 21:07:47 -0400

    Initial Commit

---
+++ MagicEightBall/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MagicEightBall
+//
+//  Created by Eric Morales on 2/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
